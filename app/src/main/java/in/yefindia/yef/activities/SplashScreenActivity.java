e0a2781c57b7faee842783a912b37ddbbb05bc0a
Shashank Shinde <sha44.shinde@gmail.com>
2018-05-17 20:20:44 +0530

    Initial commit

---
+++ app/src/main/java/in/yefindia/yef/activities/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package in.yefindia.yef.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.yefindia.yef.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
---
+++ app/src/main/java/in/yefindia/yef/utils/Utils.java
@@ -0,0 +1,7 @@
+package in.yefindia.yef.utils;
+/*
+ * Created by Shashank Shinde.
+ */
+
+public class Utils {
+}
