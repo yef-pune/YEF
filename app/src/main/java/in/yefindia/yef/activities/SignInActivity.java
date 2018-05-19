bddaa3b4b7c58d33e9cf589ed996d7cfac93a36a
Sourabh <sgsourabh30@gmail.com>
2018-05-20 00:23:03 +0530

    Sign in Activity Incomplete Code

--- app/src/main/java/in/yefindia/yef/activities/SignInActivity.java
+++ app/src/main/java/in/yefindia/yef/activities/SignInActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import in.yefindia.yef.R;
 
@@ -12,16 +14,28 @@ public class SignInActivity extends AppCompatActivity {
 
 
     TextInputLayout editTextEmail,editTextPassword;
+    Button signinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         editTextEmail=findViewById(R.id.editText_emailSignIn);
-        editTextPassword=findViewById(R.id.editText_passwordSignIn);
-
+        signinButton=(Button)findViewById(R.id.button_signIn);
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptLogin();
+            }
+        });
 //        To get editText content user getEditText methood like
+
+
+    }
+
+    private void attemptLogin() {
         String email=editTextEmail.getEditText().getText().toString();
+        Toast.makeText(getApplicationContext(),""+email,Toast.LENGTH_SHORT).show();
     }
 
     public void gotoSignUp(View view) {
