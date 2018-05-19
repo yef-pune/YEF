4f173f1087330afc478642b092671e5dbda98889
Sourabh <sgsourabh30@gmail.com>
2018-05-20 00:17:02 +0530

    Model class for User Information to be stored to the Firebase Real Time Database

---
+++ app/src/main/java/in/yefindia/yef/activities/UserDetailsModel.java
@@ -0,0 +1,16 @@
+package in.yefindia.yef.activities;
+
+public class UserDetailsModel {
+
+    String fullName,contactNumber,state,city,email;
+
+    public UserDetailsModel(String fullName, String contactNumber, String state, String city,String email) {
+        this.fullName = fullName;
+        this.contactNumber = contactNumber;
+        this.state = state;
+        this.city = city;
+        this.email=email;
+    }
+
+
+}
