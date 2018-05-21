37ddccb1af4b67a13b6d23d6028d60e09d4a289f
Sourabh <sgsourabh30@gmail.com>
2018-05-21 11:21:43 +0530

    Added common methods

--- app/src/main/java/in/yefindia/yef/utils/Utils.java
+++ app/src/main/java/in/yefindia/yef/utils/Utils.java
@@ -24,4 +24,31 @@ public class Utils {
     //Registeration Message 2
     public static final String REGISTERATION_SUCESSFUL_MESSAGE_2="Please check your inbox.";
 
+
+    //Method to check Empty Fields
+    public static boolean isEmpty(String string) {
+        return string.equals("");
+    }
+
+    //Method to check domain
+    public static boolean checkDomain(String email) {
+        return (email.substring(email.indexOf('@') + 1).toLowerCase()).equals(Utils.DOMAIN);
+    }
+
+    //Method to check weather both the passwords match
+    public static boolean doPasswordsMatch(String passOne, String conPass) {
+        return passOne.equals(conPass);
+    }
+
+    //Method to check password length (Current Length: 6)
+    public static boolean checkPasswordLength(String pass) {
+        return pass.length() >= 6;
+    }
+
+    //Method to check contact number length
+    public static boolean checkContctNumberLength(String contactNumber) {
+        return contactNumber.length() == 10;
+    }
+
+
 }
