0157d1e9ed1bf5c98c53bebc50145d1876b5fcfe
Sourabh <sgsourabh30@gmail.com>
2018-05-25 00:34:28 +0530

    email verification bug removed

--- app/src/main/java/in/yefindia/yef/activities/HomeActivity.java
+++ app/src/main/java/in/yefindia/yef/activities/HomeActivity.java
@@ -2,25 +2,68 @@ package in.yefindia.yef.activities;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import in.yefindia.yef.R;
 
 public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "HomeActivity";
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         setupupFirebseAuth();
+
+        setupNavigationView();
+    }
+
+
+    private void setupNavigationView() {
+        drawerLayout=findViewById(R.id.drawerHomeNavigation);
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_drawer);
+
+
+        navigationView=findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item)
+            {
+                // set item as selected to persist highlight
+                item.setChecked(true);
+                // close drawer when item is tapped
+                drawerLayout.closeDrawers();
+                // Add code here to update the UI based on the item selected
+                // For example, swap UI fragments here
+                return true;
+            }
+        });
+
     }
 
     @Override
@@ -65,6 +108,18 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         };
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
